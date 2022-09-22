53c43a2309f14ec5eb7eadfe1506c080fb9c5b19
nijasmon.ah@phases.dk <nijasmon.ah@phases.dk>
2022-09-22 15:43:55 +0530

    first commit

---
+++ SSOWithUmbraco.Core/SSO/BackOffice/Github/GitHubMemberAuthenticationExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Web.BackOffice.Security;
+using Umbraco.Extensions;
+
+namespace SSOWithUmbraco.Core.SSO.BackOffice.Github
+{
+    public static class GitHubBackofficeAuthenticationExtensions
+    {
+        public static IUmbracoBuilder AddGitHubBackofficeAuthentication(this IUmbracoBuilder builder)
+        {
+            // Register OpenIdConnectBackOfficeExternalLoginProviderOptions here rather than require it in startup
+            builder.Services.ConfigureOptions<GitHubBackOfficeExternalLoginProviderOptions>();
+
+
+            builder.AddBackOfficeExternalLogins(logins =>
+            {
+                logins.AddBackOfficeLogin(
+                    backOfficeAuthenticationBuilder =>
+                    {
+                        backOfficeAuthenticationBuilder.AddGitHub(
+
+                            backOfficeAuthenticationBuilder.SchemeForBackOffice(GitHubBackOfficeExternalLoginProviderOptions.SchemeName),
+                                options =>
+                                {
+                                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                                    options.ClientId = "client-id";
+                                    options.ClientSecret = "client-secret";
+                                    options.Events.OnTicketReceived = ctx =>
+                                    {
+                                        var username = ctx.Principal.FindFirstValue(ClaimTypes.Email)
+                                                ?? ctx.Principal.FindFirstValue("email")
+                                                ?? throw new Exception("Missing email claim");
+                                        if (username != null && ctx.Principal?.Identity is ClaimsIdentity claimsIdentity)
+                                        {
+                                            claimsIdentity.AddClaim(
+                                                new Claim(
+                                                    ClaimTypes.Email,
+                                                    username
+                                                )
+                                            );
+                                        }
+
+                                        return Task.CompletedTask;
+                                    };
+                                });
+
+                    });
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ SSOWithUmbraco.Core/SSO/BackOffice/Github/GitHubMemberExternalLoginProviderOptions.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Web.BackOffice.Security;
+
+namespace SSOWithUmbraco.Core.SSO.BackOffice.Github
+{
+    public class GitHubBackOfficeExternalLoginProviderOptions : IConfigureNamedOptions<BackOfficeExternalLoginProviderOptions>
+    {
+        public const string SchemeName = "GitHub";
+        public void Configure(string name, BackOfficeExternalLoginProviderOptions options)
+        {
+            if (name != "Umbraco." + SchemeName)
+            {
+                return;
+            }
+
+            Configure(options);
+        }
+
+        public void Configure(BackOfficeExternalLoginProviderOptions options)
+        {
+            options.ButtonStyle = "btn-danger";
+            options.Icon = "fa fa-cloud";
+            options.AutoLinkOptions = new ExternalSignInAutoLinkOptions(
+                // must be true for auto-linking to be enabled
+                autoLinkExternalAccount: true,
+
+                // Optionally specify default user group, else
+                // assign in the OnAutoLinking callback
+                // (default is editor)
+                defaultUserGroups: new[] { Constants.Security.AdminGroupAlias },
+
+                // Optionally specify the default culture to create
+                // the user as. If null it will use the default
+                // culture defined in the web.config, or it can
+                // be dynamically assigned in the OnAutoLinking
+                // callback.
+
+                defaultCulture: "en-US",
+                // Optionally you can disable the ability to link/unlink
+                // manually from within the back office. Set this to false
+                // if you don't want the user to unlink from this external
+                // provider.
+                allowManualLinking: true
+            )
+            {
+                // Optional callback
+                OnAutoLinking = (autoLinkUser, loginInfo) =>
+                {
+                    // You can customize the user before it's linked.
+                    // i.e. Modify the user's groups based on the Claims returned
+                    // in the externalLogin info
+                    autoLinkUser.IsApproved = true;
+                },
+                OnExternalLogin = (user, loginInfo) =>
+                {
+                    // You can customize the user before it's saved whenever they have
+                    // logged in with the external provider.
+                    // i.e. Sync the user's name based on the Claims returned
+                    // in the externalLogin info
+
+                    return true; //returns a boolean indicating if sign in should continue or not.
+                }
+            };
+
+            // Optionally you can disable the ability for users
+            // to login with a username/password. If this is set
+            // to true, it will disable username/password login
+            // even if there are other external login providers installed.
+            options.DenyLocalLogin = false;
+
+            // Optionally choose to automatically redirect to the
+            // external login provider so the user doesn't have
+            // to click the login button. This is
+            options.AutoRedirectLoginToExternalProvider = false;
+        }
+
+    }
+}
